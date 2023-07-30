c66221b9092ba20925a5c6fbeef80a3ec092ebee
kucira <kucira@users.noreply.github.com>
2023-07-30 04:55:27 +0000

    feat: for demo

--- src/components/shared/Title.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <a-typography-title>{{ title }}</a-typography-title>
-</template>
-
-<script setup lang="ts">
-defineProps<{ title: string }>()
-</script>
-
-<style scoped></style>
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,35 +1,11 @@
-export type CardType = {
-  key: number;
-  username: string;
-  cardName: string;
-  price: string;
-  email: string;
-  picture?: string;
+const getCardList = (): any[] => {
+  return [];
 };
 
-const getCardList = (): CardType[] => {
-  const cards: CardType[] = JSON.parse(
-    localStorage.getItem("database") as string
-  );
-  return cards || [];
+const postCardData = (card: any): void => {
 };
 
-const postCardData = (card: CardType): void => {
-  const cards: CardType[] =
-    JSON.parse(localStorage.getItem("database") as string) || [];
-  cards.push(card);
-
-  localStorage.setItem("database", JSON.stringify(cards));
-};
-
-const getCardListByCardName = (cardName: string): CardType => {
-  const cards: CardType[] = JSON.parse(
-    localStorage.getItem("database") as string
-  );
-
-  return cards.find(
-    (data: CardType) => data.cardName.toLowerCase() === cardName.toLowerCase()
-  ) as CardType;
+const getCardListByCardName = (cardName: string): any => {
 };
 
 export { getCardList, postCardData, getCardListByCardName };
